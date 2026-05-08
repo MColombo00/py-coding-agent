f3f223baff53f21912f2a7805752388a1c2e4e48
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2026-05-07 20:52:26 -0400

    Added original files from pc

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from python-coding-agent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
-
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model ='gemini-2.5-flash', contents="how are potatoes grown, use one paragraph maximum"
+    )
+print(response.text)
+print(response.prompt_token_count)
+print(response.candidates_token_count)
