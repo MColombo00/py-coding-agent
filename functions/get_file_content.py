7f2ec0822791990db6cb59223bec0e42dd734249
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2026-05-08 21:16:29 -0400

    Started the write_file module to allow agent to write to files in selected directory.

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,7 +1,5 @@
 import os
-import config
-
-MAX_CHARS = config.MAX_CHARS
+from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
@@ -12,10 +10,13 @@ def get_file_content(working_directory, file_path):
     if not os.path.isfile(abs_file_path):
         return f'Error: Cannot read "{file_path}" is not a file'
 
-    f = open(abs_file_path, "r")
-    content = f.read(MAX_CHARS)
-    if f.read(1):
-        content += f' [...File "{file_path}" truncated at {MAX_CHARS} characters]'
-    
-    return f"{content}"
-    
+    content = ""
+    try:
+        with open(abs_file_path, "r") as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        return f"{content}"
+    except Exception as e:
+        return f"Exception reading file: {e}"
---
+++ functions/write_file.py
@@ -0,0 +1,13 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(abs_working_dir, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(abs_file_path):
+        return f'Error: Cannot read "{file_path}" is not a file'
+    if  os.path.isdir(abs_file_path):
+        return f'Error: Cannot write to "{file_path}" as it is a directory'
+    
