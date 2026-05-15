47468b65f2e662210d32202e5cdc1ffcc3c8d5eb
MColombo00 <48252195+MColombo00@users.noreply.github.com>
2026-05-14 20:59:58 -0400

    Finished the write_file function, all test scenarios ran as planned.

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,14 +5,22 @@ def write_file(working_directory, file_path, content):
     abs_file_path = os.path.abspath(os.path.join(abs_working_dir, file_path))
 
     if not abs_file_path.startswith(abs_working_dir):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(abs_file_path):
-        return f'Error: Cannot read "{file_path}" is not a file'
-    if  os.path.isdir(abs_file_path):
-        return f'Error: Cannot write to "{file_path}" as it is a directory'
+        return f'Error: "{file_path}" is not in the working directory.'
     
     if os.path.isdir(abs_file_path):
-        os.makedirs(abs_file_path, exist_ok=True)
+        return f'Error: Cannot write to "{file_path}" as it is a directory'
 
-    with open(file_path, "w") as f:
-        f.write(content)
+    parent_dir = os.path.dirname(abs_file_path)
+
+    if parent_dir:
+        pass
+    else:
+        os.makedirs(parent_dir, exist_ok=True)
+
+    try:
+        with open(abs_file_path, "w") as f:
+            f.write(content)
+
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Exception reading file: {e}"
--- test_write_file.py
+++ test_write_file.py
@@ -4,4 +4,4 @@ print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
 
 print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
 
-# print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
